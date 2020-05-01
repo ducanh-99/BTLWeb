d9c4157aa13312798c1de70ed758e86913762a0e
anhptvnist <anhpt.vnist@gmail.com>
2020-05-01 20:55:44 +0700

    Add Theme home

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 //Đăng nhập
 Route::get('/login','LoginController@login');
 Route::get('/login-check','LoginController@login_check');
