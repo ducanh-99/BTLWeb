ec35d74bccaa6e5d5af6c4d38ccccf012335f16a
quanganh991 <dinhqanh2012@gmail.com>
2020-05-04 00:19:52 +0700

    Login.Blade.PhP update

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <div><!--login form-->
     <h2>Đăng nhập tài khoản</h2>
     <form action="{{URL::to('/login-check')}}" method="GET">
@@ -12,17 +12,3 @@
         <button type="submit" class="btn btn-default">Đăng nhập</button>
     </form>
 </div><!--/login form-->
-=======
-
-                <form action="{{URL::to('/login-check')}}" method="get">
-                    <div class="form-group">
-                      <input id="email_modal" name="email_account" type="text" placeholder="Email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                      <input id="password_modal"name="password_account" type="password" placeholder="password" class="form-control">
-                    </div>
-                    <p class="text-center">
-                      <button type="submit" class="btn btn-template-outlined"><i class="fa fa-sign-in"></i> Log in</button>
-                    </p>
-                </form>
->>>>>>> 4a7a276a730d55c51154f9f38a56162c740d034a
