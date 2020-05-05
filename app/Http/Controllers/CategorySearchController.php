4afadc953be883eb69d0696ab3ea65db337b3a91
ducanh-99 <55153541+ducanh-99@users.noreply.github.com>
2020-05-05 17:38:26 +0700

    add css to show catalog and product

--- app/Http/Controllers/CategorySearchController.php
+++ app/Http/Controllers/CategorySearchController.php
@@ -5,6 +5,7 @@ use DB,Session;
 
 use Illuminate\Http\Request;
 
+
 class CategorySearchController extends Controller
 {
     public function mainSearch(){
@@ -16,13 +17,25 @@ class CategorySearchController extends Controller
 
     public function branchSearch($id_main){
         $branchSearch = DB::table('category_branch')->where('id_category_main', $id_main)->get();
+        // get desription of category_main
+        $idMain = $branchSearch[0]->id_category_main;
+        $descriptionMain = DB::table('category_main')->select('name', 'description')->where('id_category_main', $idMain)->first();
+        return view('users.category.category_branch_list')
+            ->with('branchSearch',$branchSearch)
+            ->with('descriptionMain', $descriptionMain)
+        ;
 
-        return view('users.category.category_branch_list')->with('branchSearch',$branchSearch);
     }
 
     public function productSearch($id_branch){
+        //select name, image, id_main, description
+        $nameBranch = DB::table('category_branch')->select('name', 'image', 'id_category_main','descriptionf')->where('id_category_branch',$id_branch)->first();
+        
+        $nameMain = DB::table('category_main')->select('name')->where('id_category_main', $nameBranch->id_category_main)->first();
+        
         $productSearch = DB::table('product')->where('id_category_branch', $id_branch)->get();
-
-        return view('users.category.product_list')->with('productSearch',$productSearch);
+        // url get image of product
+        $url = $nameMain->name . '/' . $nameBranch->name . '/';
+        return view('users.category.product_list',['productSearch'=>$productSearch, 'url'=>$url, 'nameBranch'=>$nameBranch]);
     }
 }
