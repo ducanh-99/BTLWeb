e5436e09a656920c3db60b618f24ca0fb8b8797f
quanganh991 <dinhqanh2012@gmail.com>
2020-05-27 23:45:50 +0700

    fix UI Admin

--- resources/views/admin/orderManagement/view_order.blade.php
+++ resources/views/admin/orderManagement/view_order.blade.php
@@ -11,60 +11,61 @@
                     <div class="card-body">
                         <table id="example2" class="table table-bordered table-hover">
 
-                        <thead>
-        <tr>
-            <th>id_oder</th>
-            <th>id_customer</th>
-            <th>Ngày đặt hàng</th>
-            <th>Phê duyệt</th>
-            <th>Ghi chú</th>
-            <th>Xem chi tiết</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($allOrder as $eachOrder)
-            <tr>
-                <td>{{ $eachOrder->id_oder }}</td>
-                <td>{{ $eachOrder->id_customer }}</td>
-                <td>{{ $eachOrder->date }}</td>
-                <td>{{ $eachOrder->isapproved }}
+                            <thead>
+                            <tr>
+                                <th>id_oder</th>
+                                <th>id_customer</th>
+                                <th>Ngày đặt hàng</th>
+                                <th>Phê duyệt</th>
+                                <th>Ghi chú</th>
+                                <th>Xem chi tiết</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($allOrder as $eachOrder)
+                                <tr>
+                                    <td>{{ $eachOrder->id_oder }}</td>
+                                    <td>{{ $eachOrder->id_customer }}</td>
+                                    <td>{{ $eachOrder->date }}</td>
+                                    <td>{{ $eachOrder->isapproved }}
 
-                    <?php
-                    if($eachOrder->isapproved == 0){    //chưa duyệt
-                    ?>
+                                        <?php
+                                        if($eachOrder->isapproved == 0){    //chưa duyệt
+                                        ?>
 
-                    <a href="{{URL::to('/approve-order/'.$eachOrder->id_oder)}}">Duyệt</a>
-                    <a href="{{URL::to('/unapprove-order/'.$eachOrder->id_oder)}}">Hủy</a>
+                                        <a href="{{URL::to('/approve-order/'.$eachOrder->id_oder)}}">Duyệt</a>
+                                        <a href="{{URL::to('/unapprove-order/'.$eachOrder->id_oder)}}">Hủy</a>
 
-                    <?php
-                    }else if($eachOrder->isapproved==1){ //đã duyệt
-                    ?>
+                                        <?php
+                                        }else if($eachOrder->isapproved == 1){ //đã duyệt
+                                        ?>
 
-                    <a href="{{URL::to('/succeed-order/'.$eachOrder->id_oder)}}">Giao thành công</a>
+                                        <a href="{{URL::to('/succeed-order/'.$eachOrder->id_oder)}}">Giao thành công</a>
 
-                    <?php
-                    } else if($eachOrder->isapproved==2){ //giao thành công
-                    ?>
+                                        <?php
+                                        } else if($eachOrder->isapproved == 2){ //giao thành công
+                                        ?>
 
-                    <p>Đã giao hàng thành công</p>
+                                        <p>Đã giao hàng thành công</p>
 
-                    <?php
-                    }  else if($eachOrder->isapproved==3){ //bị hủy
-                    ?>
+                                        <?php
+                                        }  else if($eachOrder->isapproved == 3){ //bị hủy
+                                        ?>
 
-                    <p>Đơn hàng bị hủy</p>
+                                        <p>Đơn hàng bị hủy</p>
 
-                    <?php
-                    }
-                    ?>
-                </td>
-                <td>{{ $eachOrder->note }}</td>
-                <td><a href="{{URL::to('/view-order-detail/'.$eachOrder->id_oder)}}">Xem chi tiết</a></td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+                                        <?php
+                                        }
+                                        ?>
+                                    </td>
+                                    <td>{{ $eachOrder->note }}</td>
+                                    <td><a href="{{URL::to('/view-order-detail/'.$eachOrder->id_oder)}}">Xem chi
+                                            tiết</a></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/admin/orderManagement/view_order_detail.blade.php
+++ resources/views/admin/orderManagement/view_order_detail.blade.php
@@ -1,32 +1,45 @@
-<div>
-    Liệt kê Order Chi tiết
-</div>
-<div class="table-responsive">
-    <table class="table table-striped b-t b-light">
-        <thead>
-        <tr>
-            <th>id_oder_detail</th>
-            <th>STT</th>
-            <th>id_oder</th>
-            <th>id_product</th>
-            <th>Số lượng</th>
-            <th>Số tháng thuê</th>
-            <th>Sửa</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($allOrderDetail as $eachOrderDetail)
-            <tr>
-                <td>{{ $eachOrderDetail->id_oder_detail }}</td>
-                <td>{{ $eachOrderDetail->item_order }}</td>
-                <td>{{ $eachOrderDetail->id_oder }}</td>
-                <td>{{ $eachOrderDetail->id_product }}</td>
-                <td>{{ $eachOrderDetail->quantity }}</td>
-                <td>{{ $eachOrderDetail->discount }}</td>
-                <td>{{ $eachOrderDetail->months }}</td>
-                <td><a href="{{URL::to('/edit-order-detail/'.$eachOrderDetail->id_oder_detail)}}">Sửa mặt hàng</a></td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-</div>
+@extends('admin.welcomeAdmin')
+@section('all_branch_category')
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">View All Users' Orders</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example2" class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th>id_oder_detail</th>
+                                <th>STT</th>
+                                <th>id_oder</th>
+                                <th>id_product</th>
+                                <th>Số lượng</th>
+                                <th>Số tháng thuê</th>
+                                <th>Sửa</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($allOrderDetail as $eachOrderDetail)
+                                <tr>
+                                    <td>{{ $eachOrderDetail->id_oder_detail }}</td>
+                                    <td>{{ $eachOrderDetail->item_order }}</td>
+                                    <td>{{ $eachOrderDetail->id_oder }}</td>
+                                    <td>{{ $eachOrderDetail->id_product }}</td>
+                                    <td>{{ $eachOrderDetail->quantity }}</td>
+                                    <td>{{ $eachOrderDetail->discount }}</td>
+                                    <td>{{ $eachOrderDetail->months }}</td>
+                                    <td><a href="{{URL::to('/edit-order-detail/'.$eachOrderDetail->id_oder_detail)}}">Sửa
+                                            mặt hàng</a></td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
